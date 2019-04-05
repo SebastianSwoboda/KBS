df9b152e03b263b7b4c07829a55fd4a669426961
SebastianSwoboda <35917298+JoelRauber@users.noreply.github.com>
2019-04-05 16:31:44 +0200

    added link to register view from login view

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../ui/kbs_ui_login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../ui/kbs_ui_register.fxml"));
         primaryStage.setTitle("KBS Login");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
