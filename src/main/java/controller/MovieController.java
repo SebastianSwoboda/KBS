fa26b3e7cd92da11279aa470dd7c549f4bcbeaec
SilvioMerz <silvio.merz01@gmail.com>
2019-05-10 14:54:03 +0200

    Designed the main page and added movies to database

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -3,6 +3,7 @@ package controller;
 import factory.CreateStageFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class MovieController {
 
     Stage stage;
     CreateStageFactory createStageFactory = new CreateStageFactory();
+
+
 
 
     @FXML
